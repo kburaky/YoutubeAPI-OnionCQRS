1f5842c604d516f57c9347286dd8d993e77bd97f
kburaky <kburak.ylmz@gmail.com>
2024-12-08 00:13:27 +0300

    EF Core Code First ile Migration ve Veritabanı oluşturma işlemleri

--- Presentation/HepsiAPI.API/Program.cs
+++ Presentation/HepsiAPI.API/Program.cs
@@ -1,4 +1,5 @@
-using System.Runtime.InteropServices;
+using HepsiAPI.Persistence;
+using YoutubeApi.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,10 +12,15 @@ builder.Services.AddSwaggerGen();
 
 
 var env = builder.Environment;
+
 builder.Configuration
     .SetBasePath(env.ContentRootPath)
     .AddJsonFile("appsettings.json",optional:false)
     .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+
+builder.Services.AddPersistence(builder.Configuration);
+
+
 
 var app = builder.Build();
 
