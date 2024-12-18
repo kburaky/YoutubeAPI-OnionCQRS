bb865724cbcbcbf3465dbc8228cd3cbff9b3ffc7
kburaky <kburak.ylmz@gmail.com>
2024-12-19 00:49:28 +0300

    Authentication - Authorization #3 | JWT Konfigürasyonları ve Token Servis #2

--- Presentation/HepsiAPI.API/Program.cs
+++ Presentation/HepsiAPI.API/Program.cs
@@ -1,7 +1,9 @@
 using HepsiAPI.Persistence;
 using HepsiAPI.Application;
+using HepsiAPI.Infrastructure;
 using HepsiAPI.Mapper;
 using HepsiAPI.Application.Exceptions;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -24,8 +26,40 @@ builder.Configuration
     .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
 
 builder.Services.AddPersistence(builder.Configuration);
+builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddApplication();
 builder.Services.AddCustomMapper();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Hepsi API", Version = "v1", Description = "Hepsi API swagger client." });
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "'Bearer' yazýp boþluk býraktýktan sonra Token'ý Girebilirsiniz \r\n\r\n Örneðin: \"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9\""
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+
+    });
+});
+
+
 
 var app = builder.Build();
 
